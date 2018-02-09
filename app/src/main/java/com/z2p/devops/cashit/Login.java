e0ccba063eff9ea33490a0e20eead95cf2b21dfd
imedaouidene <imed.aouidene@gmail.com>
2018-02-09 19:51:49 +0100

    Initial commit

---
+++ app/src/main/java/com/z2p/devops/cashit/Login.java
@@ -0,0 +1,25 @@
+package com.z2p.devops.cashit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+
+
+
+
+
+    }
+
+}
